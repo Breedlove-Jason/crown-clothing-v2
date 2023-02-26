eedfaecf0fca51dce1af8f4d564ffc6b74d2bd2f
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-02-26 12:34:58 -0800

    created categories.component.jsx

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import React from 'react';
-import './categories.styles.scss';
-import './components/category-item/category-item.style.scss'
-import CategoryItem from './components/category-item/category-item.component';
+import Categories from "./components/categories/categories.component";
 
 function App() {
-    const categories = [
-        {
-            "id": 1,
-            "title": "Hats",
-            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },
-        {
-            "id": 2,
-            "title": "Jackets",
-            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-        },
-        {
-            "id": 3,
-            "title": "Sneakers",
-            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-        },
-        {
-            "id": 4,
-            "title": "Women's",
-            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },
-        {
-            "id": 5,
-            "title": "Men's",
-            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-
-    ];
 
 
     return (
-        <div className={"categories-container"} >
-            {categories.map((category) => (
-                <CategoryItem key={category.id} category={category} />
-
-            ))}
-
+        <div>
+            <Categories/>
         </div>
     );
 }
