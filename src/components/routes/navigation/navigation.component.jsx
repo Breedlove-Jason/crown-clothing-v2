8979e368299255226c4e8d46980d74321e506422
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-02-26 15:22:40 -0800

    imported crown log and basic nav styling

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,12 +1,18 @@
 import {Outlet, Link} from "react-router-dom";
 import React from "react";
 import {Fragment} from "react";
+import {ReactComponent as CrwnLogo} from "../../../assets/crown.svg";
+import './navigation.styles.scss';
 
 function Navigation() {
     return (
         <Fragment>
             <div className={'navigation'}>
-                <Link className={'logo-container'} to={'/'}><div>Logo</div> </Link>
+                <Link className={'logo-container'} to={'/'}>
+                    <div>
+                        <CrwnLogo className={'logo'} />
+                    </div>
+                </Link>
                 <div className="nav-links-container"></div>
                 <Link className={'nav-link'} to={'/shop'}>SHOP</Link>
 
