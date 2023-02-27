4747521fc63319db31721d4b2ddbdebd7320bf39
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-02-27 13:15:48 -0800

    added state to handle user input on sign-up-form.component.jsx

--- src/components/routes/sign=in/sign-in.component.jsx
+++ src/components/routes/sign=in/sign-in.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import {signInWithGooglePopup, createUserDocumentFromAuth} from "../../../utils/firebase/firebase.utils";
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth
+} from "../../../utils/firebase/firebase.utils";
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
@@ -7,10 +11,12 @@ const SignIn = () => {
         const userDocRef = await createUserDocumentFromAuth(user)
     };
 
+
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In With Google</button>
+            <SignUpForm/>
         </div>
     );
 };
